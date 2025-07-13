d72e914d1f3c4787a6ba708cd19566e60b7b941e
gozoooom <sheng.tp.tw@gmail.com>
2025-07-12 17:36:31 -0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{ts,tsx}",
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}"
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3758F9",     // your blue brand color
+        secondary: "#8899A8",   // example gray
+        light: "#FFFFFF",       // used in bg-light
+        dark: "#637381",        // used in text-dark
+      },
+    },
   },
   plugins: [],
-}
+};
